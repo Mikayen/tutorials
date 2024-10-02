16f4ddce7d5307cd04b1067b02c946c47eb403b8
Minh Khanh Nguyen <minh270402@gmail.com>
2024-10-02 09:29:45 +0200

    test

--- tutorials/src/01-img-manipulation.problem.py
+++ tutorials/src/01-img-manipulation.problem.py
@@ -4,22 +4,28 @@
 # Load, resize and rotate an image. And display it to the screen.
 
 # TODO First step is to import the opencv module which is called 'cv2'
-
+import cv2 as cv
 # TODO Check the opencv version
-
+print(cv.__version__)
 # TODO Load an image with image reading modes using 'imread'
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
 #                         orientation.
+
 # cv2.IMREAD_GRAYSCALE  - If set, always convert image to the single channel
 #                         grayscale image (codec internal conversion).
+
 # cv2.IMREAD_COLOR      - If set, always convert image to the 3 channel BGR
 #                         color image.
-
+img = cv.imread("./tutorials/data/images/logo.png", cv.IMREAD_COLOR)
 # TODO Resize image with 'resize'
-
+img = cv.resize(img, (100, 100))
 # TODO Rotate image (but keep it rectangular) with 'rotate'
-
+img = cv.rotate(img, cv.ROTATE_180)
 # TODO Save image with 'imwrite'
 
 # TODO Show the image with 'imshow'
+win_name = "FENSTER"
+cv.namedWindow(win_name, cv.WINDOW_AUTOSIZE)
+cv.imshow(win_name, img)
+cv.waitKey(0)
--- tutorials/src/02-pixel-manipulation.problem.py
+++ tutorials/src/02-pixel-manipulation.problem.py
@@ -4,28 +4,41 @@
 # Direct pixel access and manipulation. Set some pixels to black, copy some part of the image to some other place,
 # count the used colors in the image
 
-import cv2
+import cv2 as cv
 import numpy as np
 
 # TODO Loading images in grey and color
+img_gray = cv.imread("./tutorials/data/images/logo.png", cv.IMREAD_GRAYSCALE)
+img_color = cv.imread("./tutorials/data/images/logo.png", cv.IMREAD_COLOR)
 
+img = img_color
 # TODO Do some print out about the loaded data using type, dtype and shape
+print(type(img))
+print(img.dtype)
+print(img.shape)
 
 # TODO Continue with the grayscale image
 
 # TODO Extract the size or resolution of the image
+#bei graubild bei farb bild braucht man dritte Variable noch
+'''height,width = img.shape
+print(height)
+print(width)'''
 
 # TODO Resize image
+#img = cv.resize(img, (8,5))
+
 
 # Row and column access, see https://numpy.org/doc/stable/reference/arrays.ndarray.html for general access on ndarrays
 # TODO Print first row
+#print(img[0])
 
 # TODO Print first column
-
+#print(img[:,0])
 # TODO Continue with the color image
 
 # TODO Set an area of the image to black
-
+img[100:150, 20:80] = 0
 # TODO Show the image and wait until key pressed
 
 # TODO Find all used colors in the image
@@ -35,5 +48,10 @@ import numpy as np
 # TODO Save image to a file
 
 # TODO Show the image again
+win_name = "FENSTER"
+cv.namedWindow(win_name, cv.WINDOW_AUTOSIZE)
+cv.imshow(win_name, img)
+cv.waitKey(0)
+
 
 # TODO Show the original image (copy demo)
